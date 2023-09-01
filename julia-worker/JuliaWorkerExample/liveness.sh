031da33432025f09a9e103ae0e22df6fa0f70514
Jakub Wronowski <jakubwro@gmail.com>
2023-09-01 10:32:52 +0200

    liveness probe

--- julia-worker/JuliaWorkerExample/liveness.sh
+++ julia-worker/JuliaWorkerExample/liveness.sh
@@ -1,9 +1,14 @@
 
-time_ago=$(date -d 'now - 10 seconds' +%s)
+LIVENESS_PROBE_TIMEOUT_SECONDS="${LIVENESS_PROBE_TIMEOUT_SECONDS:-10}"
+
+time_ago=$(date -d 'now - $LIVENESS_PROBE_TIMEOUT_SECONDS seconds' +%s)
 working_since=$(date -r "/tmp/liveness/working" +%s)
 
 if [ $? -eq 0 ]; then
-    if (( working_since <= time_ago )); then
+    # File `working` exists.
+    if (( working_since < time_ago )); then
+            # File `working` was modifed before `LIVENESS_PROBE_TIMEOUT_SECONDS`.
+            # Exit with error to let liveness probe SIGTERM process.
             exit 1
     fi
 fi
